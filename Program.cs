7e1e49da8deaed01df4abcb10377407ed72bb21d
Alexey <brech_36@mail.ru>
2023-01-31 15:06:18 +0900

    add prog

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] num = {3,7,22,2,78};
+int max=0;
+int min=0;
+for(var i=0;i<num.Length;i++)
+{
+if(num[i]>max)
+{
+    max=num[i];
+}
+else if(num[i]<=num[i])
+{
+    min=num[i];
+}
+}
+string mas = string.Join(" ", num);
+Console.Write(mas);
+Console.WriteLine($"->{max-min}");
